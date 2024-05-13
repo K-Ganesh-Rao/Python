74329d54f63a4deb88a5faae52be927db309515a
Ganesha_G_Rao <130823089+K-Ganesh-Rao@users.noreply.github.com>
2024-05-13 19:25:00 +0530

    Create Highest_score.py

---
+++ Highest_score.py
@@ -0,0 +1,13 @@
+#input the list of scores
+# Calculate without max function
+student_score = input().split()
+
+for n in range(0 , len(student_score)):
+    student_score[n] = int(student_score[n])
+
+heighest_score = 0
+for score in student_score:
+    if score > heighest_score:
+        heighest_score = score
+
+print(f"The Highest score in the class lst: {heighest_score}")
